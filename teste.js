ceaffb0f44db13289ed13c77cd88768b88d60fb2
LuzCA001 <lucasxmedeiross@gmail.com>
2025-09-01 07:24:17 -0300

    começar a mecher no DB

--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-import Curso from "./Curso.js";
+import Curso from "./Models/Curso.js";
 
 const curso = new Curso("5", "JavaScript", "programação orientada a objetos", "Renato","10", "intermediario", "20", "299,90", "nada por enquanto.");
 
